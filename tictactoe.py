514805ae494b979e177c58ec752d23af4ab9d8e2
Engin Cicek <ecicek@gmail.com>
2020-01-13 16:55:22 +0300

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,26 +1,72 @@
-board=[0]*9
-players=['X','W']
+'''
+TicTacToe Game
+'''
+board=['']*9
+players=['X','O']
 turn=0
 
-def print_board(board):
-    print(' {} | {} | {} '.format(board[6],board[7],board[8]))
-    print('--------------')
-    print(' {} | {} | {} '.format(board[3],board[4],board[5]))
-    print('--------------')
-    print(' {} | {} | {} '.format(board[0],board[1],board[2]))
-
-print_board(board)
-while True:
+def init_game():
+    global board
+    global turn
     
-    pos=int(input('position :'))
-    if pos>9:
-        print('Enter number b/w 1-9')
-    elif pos==0:
-        break
-    else:
-        if board[pos-1]!=0:
-            print('Burası dolu')
+    for i in range(0,9):
+        board[i]=''
+
+    while True:
+        first_player=input('Choose X or O').upper()
+        if first_player=='X':
+            turn=0
+            break
+        elif first_player=='O':
+            turn=1
+            break
         else:
+            print('Enter either X or O')
+
+def print_board(board):
+    print(' {}  | {}  | {}'.format(board[6],board[7],board[8]))
+    print('-------------')
+    print(' {}  | {}  | {}'.format(board[3],board[4],board[5]))
+    print('-------------')
+    print(' {}  | {}  | {}'.format(board[0],board[1],board[2]))
+
+def check_for_win(board):
+    for i in range(0,9,3):
+        if board[i]==board[i+1]==board[i+2]!='':return True
+    for i in range(6,9):
+        if board[i]==board[i-3]==board[i-6]!='':return True
+    if board[6]==board[4]==board[2]!='':return True 
+    if board[0]==board[4]==board[8]!='':return True
+        
+init_game()
+print_board(board)
+
+#main loop
+while True:    
+    pos=int(input('Choose between 1..9 from dialpad or press 0 for quit'))
+    if pos==0:
+        break
+    if pos>0 and pos<10:
+        if board[pos-1]=='':
             board[pos-1]=players[turn]
-            turn=abs(turn-1)
-            print_board(board)
+            if check_for_win(board):
+                print_board(board)
+                print('Player {} won ! '.format(players[turn]))
+                res=input('Play Another ? Y/N')
+                if res=='Y':
+                    init_game()
+                    print_board(board)
+                else: break
+            elif (9-len([item for item in board if item!='']))==0:
+                print('Game Over. No Winner !') 
+                res=input('Play Another ? Y/N').lower()
+                if res=='y':
+                    init_game()
+                    print_board(board)
+                else: break
+            else:    
+                turn=abs(turn-1)
+                print(turn)
+                print_board(board)
+        else:
+            print('That box is already full. Choose another !')
